74224ebb665ea4d220d1756eb3d8d2605b1b77df
IvaRude <rudenko.ivan2014@yandex.ru>
2023-04-15 20:01:41 +0400

    Added postgres

--- backend/app/api/server.py
+++ backend/app/api/server.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from backend.app.api.routes import router as api_router
-
+from backend.app.core import config, tasks
 
 def get_application():
-    app = FastAPI(title="UnitsTest", version="1.0.0")
+    app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
 
     app.add_middleware(
         CORSMiddleware,
@@ -14,7 +14,8 @@ def get_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
-
+    app.add_event_handler("startup", tasks.create_start_app_handler(app))
+    app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
     app.include_router(api_router, prefix="/api")
 
     return app
@@ -22,3 +23,6 @@ def get_application():
 
 app = get_application()
 
+
+if __name__ == '__main__':
+    print('Heelo')
